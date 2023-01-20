fb6ec0a706a7ed9f6729ac763e6512dc103ce764
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-01-20 10:45:35 -0500

    🔨 (Ignore) — Ignored tailwind build file

--- src/components/Home/Main.tsx
+++ src/components/Home/Main.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Main() {
     return (
         <article>
-            <header>
+            <header className="flex flex-row gap-1">
                 <span>SnailyCAD</span>
-                <span id="cad_version"></span>
+                <span id="cad_version">vTest</span>
             </header>
         </article>
     );
