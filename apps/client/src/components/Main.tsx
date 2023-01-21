c6ea54a6f8e61101d1e3c9b726266d410ee82e0b
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-01-20 20:24:31 -0500

    🎉 Created base components & configured Tailwind

---
+++ apps/client/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <article>
+            <header>
+                <div className="flex flex-row gap-1">
+                    <span>SnailyCAD</span>
+                    <span>1.0.0</span>
+                </div>
+            </header>
+        </article>
+    );
+}
