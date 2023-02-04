44de43587907ff94112ec14833d56518844bbc12
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-02-04 14:35:17 -0500

    🔄️ (Client) — Restart Client

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,42 +1,18 @@
-'use client';
-import '../styles/tailwind.css';
-import Header from '@/components/Header/Header';
-import 'react-toastify/dist/ReactToastify.css';
-import Middle from '@/components/Auth/Middle';
-import { SessionProvider } from 'next-auth/react';
-let user = true; //! Testing purposes only - remove in production
-
-// Get the window.location.hostname from the browser
+import './globals.css'
 
 export default function RootLayout({
-    children,
-    session,
+  children,
 }: {
-    children: React.ReactNode;
-    session?: any;
+  children: React.ReactNode
 }) {
-    if (typeof window !== 'undefined') {
-        window.addEventListener('submit', (e) => {
-            e.preventDefault();
-        });
-    }
-    return (
-        <html lang="en">
-            {/*
+  return (
+    <html lang="en">
+      {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-            <head />
-            <body className="h-screen">
-                <SessionProvider session={session}>
-                    <div className="h-full flex flex-col flex-grow">
-                        <Header />
-                        <Middle session={session}>
-                            <>{children}</>
-                        </Middle>
-                    </div>
-                </SessionProvider>
-            </body>
-        </html>
-    );
+      <head />
+      <body>{children}</body>
+    </html>
+  )
 }
--- apps/client/src/components/Info/AppInfo.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function AppInfo() {
-    return <div>Created by Whitigol Web Design</div>;
-}
