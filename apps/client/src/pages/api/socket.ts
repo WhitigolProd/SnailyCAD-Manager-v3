f87fe6d2122146808c1a9dad04ae80856cd22b25
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-02-09 13:24:16 -0500

    🔨 (Socket) — Add types

--- apps/client/src/pages/api/socket.ts
+++ apps/client/src/pages/api/socket.ts
@@ -1,6 +1,6 @@
 // I have socket.io and socket.io-client installed. Not sure what to do beyond that. I do a GET request on the client to start the socket server.
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler() {}
+export default function handler(req: NextApiRequest, res: NextApiResponse) {}
 
 // export {serverSocket} // I want to export the server socket when it's created, so can call it in other files.
